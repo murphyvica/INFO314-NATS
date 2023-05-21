348ef8ced14218f12af5d49df4df1dd86973c51d
Zac <zkornas@uw.edu>
2023-05-20 18:50:50 -0700

    Updating Broker file

--- Java/StockBrokerClient.java
+++ Java/StockBrokerClient.java
@@ -1,5 +1,11 @@
+import java.io.*;
+import io.nats.client.*;
+
 public class StockBrokerClient {
     //Connect to NATS Server
+
+    Connection nc = Nats.connect("nats://localhost:4222");
+
     //Have portfolio of stocks
     //Monitor stocks of interest
     //Have set of conditions for when to buy and sell stocks
